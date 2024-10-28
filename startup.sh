d379bc7ff3629e94ebf00c7c93dcf3af01867d09
kishore-syren <kishore.r@syrencloud.com>
2024-10-28 15:50:12 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 python  -m  pip  install  --upgrade  pip
 pip  install  -r  requirements.txt
-gunicorn  -b  0.0.0.0:${WEBSITES_PORT:-8000} -w  4  --timeout  600  app:app
+gunicorn  -b  0.0.0.0:${WEBSITES_PORT} -w  4  --timeout  600  app:app
